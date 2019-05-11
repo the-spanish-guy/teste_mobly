0f2aaa9920869b84be5d93b6cee9d429b5542880
the-spanish-guy <32144508+the-spanish-guy@users.noreply.github.com>
2019-05-10 23:55:38 -0300

    página inicial do projeto
    Esta é a página inicial do projeto que será mostrada assim que abrir o sistema

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row center-align">
+			<div class="col s12 m4">
+				<a href="time.php"><img src="img/escudo.png" alt=""></a>
+				Inserir Times
+			</div>
+			<div class="col s12 m4">
+				<a href="jogador.php"><img src="img/camisa.png" alt=""></a>
+				Inserir Jogadores
+			</div>
+			<div class="col s12 m4">
+				<a href="listar.php"><img src="img/olho2.png" alt=""></a>
+				Visualizar registros
+			</div>
+		</div>
+	</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
